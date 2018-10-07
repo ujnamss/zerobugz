6fb67b8ca3eb179628b88a97dcbb82613519f8d1
Manjunath Somashekar <ujnamss@gmail.com>
2018-10-07 00:05:56 -0700

    added support to pass count as url argument than part of the entity_json

--- zerobugz/core.py
+++ zerobugz/core.py
@@ -4,7 +4,6 @@ import requests
 
 api_key = os.getenv("ZB_API_KEY", "default")
 serverBaseUrl = os.getenv("ZB_SERVER_BASE_URL", "https://0bugz.com/api")
-variationsAPIUrl = "{}/variations".format(serverBaseUrl)
 headers = {
     'Content-type': "application/json"
 }
@@ -14,16 +13,19 @@ def load_test_cases(schema, count):
     with open(schema) as json_data:
         variationsPayload = json.load(json_data)
 
+    variations = []
     if variationsPayload:
         # Add retry logic
+        variationsAPIUrl = "{}/variations".format(serverBaseUrl)
+        variationsAPIUrl = "{}?count={}".format(variationsAPIUrl, count)
         variationsResponse = requests.post(variationsAPIUrl, headers=headers, data=json.dumps(variationsPayload))
-        assert(variationsResponse.status_code >= 200 and variationsResponse.status_code <= 300)
-        # print(variationsResponse.text)
-        # Add error conditions
-        results = json.loads(variationsResponse.text)['result']
-        assert(len(results) == count)
-        variations = []
-        for result in results:
-            variations.append((result, ))
-        return variations
-    return None
+        if variationsResponse.status_code < 200 or variationsResponse.status_code > 300:
+            variationsResponse = json.loads(variationsResponse.text)
+            assert(variationsResponse["status"] == 'failure')
+            print("zerobugz: data generation failed: {}".format(variationsResponse["result"]))
+        else:
+            results = json.loads(variationsResponse.text)['result']
+            assert(len(results) == count)
+            for result in results:
+                variations.append((result, ))
+    return variations
