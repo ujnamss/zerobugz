b6567e992a22db659455ee108aae35d537c022be
Manjunath Somashekar <ujnamss@gmail.com>
2018-10-07 02:31:06 -0700

    bumped up the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name = "zerobugz",
-    version = "0.0.1",
+    version = "0.0.2",
     author = "Manjunath Somashekar",
     author_email = "ujnamss@gmail.com",
     packages = ["zerobugz"],
